9950629e730f5a796245d86031063b83e2ce88f8
ericthecurious <hello@ericthecurious.com>
2025-12-31 19:52:56 -0800

    patch: fix export path

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { default } from './functions/generateId.js'
 export { default as generateId } from './functions/generateId.js'
-export { default as generateShortId } from './functions/generateId.js'
+export { default as generateShortId } from './functions/generateShortId.js'
